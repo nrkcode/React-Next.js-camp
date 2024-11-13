b093d806cc7fbad6e3d0e5465b9a82455a6bcbb5
narim <0018823nmn@naver.com>
2024-11-13 11:05:51 +0900

    feat: 빠진코드넣음

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,9 +3,9 @@ import { Header } from "@/components";
 function HomePage() {
     return (
         <div className="page">
-            <div className="page__container">
+            <div className="page__container bg-stone-900">
                 <Header />
-                <div className="">
+                <div className="w-full h-full flex flex-col items-center justify-start pb-6 px-6 gap-6">
                     {/* 상단 3개의 위젯 */}
                     <div className="w-full flex items-center gap-6"></div>
                     {/* 하위 2개의 위젯 */}
